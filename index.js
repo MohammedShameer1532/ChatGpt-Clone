28a97c6a340b424b1bdd579a2c75bbfceaa8c014
Mohammed Shameer <118889168+MohammedShameer1532@users.noreply.github.com>
2024-02-15 20:55:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express();
 
 app.use(bodypParser.json());
 app.use(cors({
-  origin: "https://chat-gpt-frontend.netlify.app",
+  origin: "http://localhost:5173",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
